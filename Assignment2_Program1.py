e068ebb4c4581f60addf28db1d3541ede746873a
SiegfredLorelle <siegfredlorellecmina@iskolarngbayan.pup.edu.ph>
2021-10-23 21:33:34 +0800

    prog1 first try

---
+++ Assignment2_Program1.py
@@ -0,0 +1,11 @@
+"""Create a program that will ask for name, age and address. 
+Display those details in the following format.
+Hi, my name is _____. I am ____ years old and I live in _____ .
+"""
+name    = input("Please enter your name:  ")
+age     = int(input("Please enter your age:  "))
+address = input("Please enter you address:  ")
+
+
+print (f"Hi, my name is {name}. I am {age} years old and I live in {address}. ")
+
